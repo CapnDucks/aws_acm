45fedbab9f8cba201434ca2726d5f449557a95d0
Cap'n Duck <coder@capnduck.com>
2023-12-28 15:22:18 -0800

    Fixing for tflint.

--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
 variable "validate_certificate" {
-  description = "Whether to validate certificate by creating Route53 record"
+  description = "Whether to validate certificate by creating Route53 record."
   type        = bool
   default     = true
 }
 
 variable "validation_allow_overwrite_records" {
-  description = "Whether to allow overwrite of Route53 records"
+  description = "Whether to allow overwrite of Route53 records."
   type        = bool
   default     = true
 }
 
 variable "wait_for_validation" {
-  description = "Whether to wait for the validation to complete"
+  description = "Whether to wait for the validation to complete."
   type        = bool
   default     = true
 }
 
 variable "certificate_transparency_logging_preference" {
-  description = "Specifies whether certificate details should be added to a certificate transparency log"
+  description = "Specifies whether certificate details should be added to a certificate transparency log."
   type        = bool
   default     = true
 }
 
 variable "domain_name" {
-  description = "A domain name for which the certificate should be issued"
+  description = "A domain name for which the certificate should be issued."
   type        = string
 }
 
 variable "subject_alternative_names" {
-  description = "A list of domains that should be SANs in the issued certificate"
+  description = "A list of domains that should be SANs in the issued certificate."
   type        = list(string)
   default     = []
 }
@@ -45,13 +45,13 @@ variable "validation_method" {
 }
 
 variable "create_route53_records" {
-  description = "When validation is set to DNS, define whether to create the DNS records internally via Route53 or externally using any DNS provider"
+  description = "When validation is set to DNS, define whether to create the DNS records internally via Route53 or externally using any DNS provider."
   type        = bool
   default     = true
 }
 
 variable "validation_record_fqdns" {
-  description = "When validation is set to DNS and the DNS validation records are set externally, provide the fqdns for the validation"
+  description = "When validation is set to DNS and the DNS validation records are set externally, provide the fqdns for the validation."
   type        = list(string)
   default     = []
 }
@@ -64,7 +64,7 @@ variable "dns_ttl" {
 
 variable "tags" {
   default     = null
-  description = "Tags for the resource"
+  description = "Tags for the resource."
   type        = map(string)
 }
 
@@ -76,6 +76,6 @@ variable "private_zone" {
 
 variable "aws_region" {
   default     = "us-east-1"
-  description = ""
+  description = "AWS region for the infrastructure."
   type        = string
 }
